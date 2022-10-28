43d19c4e1abad4fafa946eb9cbd98bafbfd941a8
Bric66 <115704191+Bric66@users.noreply.github.com>
2022-10-28 17:14:06 +0200

    fizzbuzz corrigé

--- routes/firstDivision.js
+++
@@ -1,9 +0,0 @@
-function firstDivision(fizzbuzzNumber, firstDivider) {
-  if (fizzbuzzNumber % firstDivider == 0) {
-    return "Fizz";
-  } else {
-    return "";
-  }
-}
-
-module.exports = firstDivision;
--- routes/secondDivision.js
+++
@@ -1,9 +0,0 @@
-function secondDivision(fizzbuzzNumber, secondDivider) {
-  if (fizzbuzzNumber % secondDivider == 0) {
-    return "Buzz";
-  } else {
-    return "";
-  }
-}
-
-module.exports = secondDivision;
