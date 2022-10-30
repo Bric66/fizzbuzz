e393a2a110595b0f0d723d43268abe02e88f3281
Bric66 <115704191+Bric66@users.noreply.github.com>
2022-10-30 12:26:10 +0100

    avec ===

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function fizzbuzz(fizzNumber) {
   if (fizzNumber < 1 || fizzNumber > 20) {
     console.log("log with a number between 1 and 20");
-  } else if (fizzNumber % 5 == 0 && fizzNumber % 3 == 0) {
+  } else if (fizzNumber % 5 === 0 && fizzNumber % 3 === 0) {
     console.log("FizzBuzz");
-  } else if (fizzNumber % 3 == 0) {
+  } else if (fizzNumber % 3 === 0) {
     console.log("Fizz");
-  } else if (fizzNumber % 5 == 0) {
+  } else if (fizzNumber % 5 === 0) {
     console.log("Buzz");
   } else {
     console.log(fizzNumber);
